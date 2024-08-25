b5c9ebb02192bd58f58c42be33d99f125268f60a
Developer YLabz <developer@ylabz.com>
2024-08-25 13:00:13 -0700

    Add needed libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.ylabz.twincam"
-        minSdk = 32
+        minSdk = 31
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
