11445d38b9a2e26a9c109485680d8080eef0b899
Ash <developer@ylabz.com>
2025-03-13 11:31:30 -0700

    Remove gbird module.

--- feature/gbird/src/main/java/com/ylabz/basepro/feature/gbird/ui/GbirdEvents.kt
+++
@@ -1,8 +0,0 @@
-package com.ylabz.basepro.feature.gbird.ui
-
-sealed class GbirdEvent {
-    object LoadGbird : GbirdEvent()
-    data class UpdateSetting(val settingKey: String, val settingValue: String) : GbirdEvent()
-    object DeleteAllEntries : GbirdEvent()  // New event to delete all entries
-}
-
