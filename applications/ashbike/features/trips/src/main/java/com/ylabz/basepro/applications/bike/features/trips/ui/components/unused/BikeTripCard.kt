3136bdb94ef28fde56638697f74d33100dee5ae4
Ash <developer@ylabz.com>
2025-04-20 16:47:22 -0700

    feat: Refactor and remove unused files in trips feature
    This commit refactors the trips feature by removing unused files and reorganizing components.
    
    Key changes:
    
    -   Renamed `BikeTripCard.kt` to `BikeTripCard_NOT_USED.kt` and moved it to the `unused` directory.
    -   Removed `CamItemRow.kt` and `DetailsRoute.kt` as they are no longer used.

--- applications/ashbike/features/trips/src/main/java/com/ylabz/basepro/applications/bike/features/trips/ui/components/unused/BikeTripCard.kt
+++ applications/ashbike/features/trips/src/main/java/com/ylabz/basepro/applications/bike/features/trips/ui/components/unused/BikeTripCard.kt
@@ -1,34 +1,22 @@
-package com.ylabz.basepro.applications.bike.features.trips.ui.components
+package com.ylabz.basepro.applications.bike.features.trips.ui.components.unused
 
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.net.Uri
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextOverflow
@@ -36,17 +24,7 @@ import androidx.compose.ui.unit.dp
 import com.ylabz.basepro.applications.bike.database.BikeProEntity
 import com.ylabz.basepro.applications.bike.features.trips.ui.TripsEvent
 
-import androidx.compose.ui.graphics.Color
-
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.graphics.painter.BitmapPainter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.core.net.toUri
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
@@ -61,7 +39,7 @@ import java.util.Locale
 
 
 @Composable
-fun BikeTripCard(
+fun BikeTripCard_NOT_USED(
     item: BikeProEntity,
     onEvent: (TripsEvent) -> Unit,
     navTo: (String) -> Unit
@@ -75,9 +53,6 @@ fun BikeTripCard(
             .clickable { navTo("details/${item.id}") },
         shape = MaterialTheme.shapes.medium,
         elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
-        colors = CardDefaults.cardColors(
-            containerColor = getPastelColor(item.id.hashCode())
-        )
     ) {
         Column(modifier = Modifier.padding(16.dp)) {
             // Header row with title and delete
@@ -92,7 +67,7 @@ fun BikeTripCard(
                     maxLines = 1,
                     overflow = TextOverflow.Ellipsis
                 )
-                IconButton(onClick = { onEvent(TripsEvent.DeleteItem(item.id)) }) {
+                IconButton(onClick = { /*onEvent(TripsEvent.DeleteItem(item.id))*/ }) {
                     Icon(imageVector = Icons.Default.Delete, contentDescription = "Delete")
                 }
             }
@@ -143,5 +118,5 @@ fun BikeTripCardPreview() {
         startLng = -118.2437,
         endLat = 34.06,
         endLng = -118.25)
-    BikeTripCard(item = item, onEvent = {}, navTo = {})
+    BikeTripCard_NOT_USED(item = item, onEvent = {}, navTo = {})
 }
