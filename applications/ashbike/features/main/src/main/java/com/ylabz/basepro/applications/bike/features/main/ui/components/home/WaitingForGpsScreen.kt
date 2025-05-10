33a39c25f1a12372a9bcbcfbe86814550e3e6caf
Ash <developer@ylabz.com>
2025-05-10 14:18:13 -0700

    feat: Add animated waiting screen for GPS location
    This commit introduces a new `WaitingForGpsScreen` composable that is displayed while the application is waiting to acquire a GPS fix.
    
    Key changes:
    
    -   Created `WaitingForGpsScreen` to handle the display while waiting for GPS.
    -   Implemented logic to check and request location permissions.
    -   Added a button to direct the user to the GPS settings if needed.
    -   Included an animated `CircularProgressIndicator` and an animated icon (`LocationSearching`) to provide visual feedback during the waiting period.
    -   Added animations for rotation, scaling, and color changes for the icon and progress indicator.
    -   Added a preview for the `WaitingForGpsScreen`.

--- applications/ashbike/features/main/src/main/java/com/ylabz/basepro/applications/bike/features/main/ui/components/home/WaitingForGpsScreen.kt
+++ applications/ashbike/features/main/src/main/java/com/ylabz/basepro/applications/bike/features/main/ui/components/home/WaitingForGpsScreen.kt
@@ -1,15 +1,15 @@
 package com.ylabz.basepro.applications.bike.features.main.ui.components.home
 
 import android.Manifest
+import android.R.attr.animation
 import android.content.Context
 import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
-import android.location.LocationManager.GPS_PROVIDER
 import android.os.Bundle
 import androidx.annotation.RequiresPermission
-import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.animateColor
 import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.RepeatMode
@@ -17,58 +17,35 @@ import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
-import androidx.compose.animation.expandVertically
-import androidx.compose.animation.fadeIn
-import androidx.compose.animation.fadeOut
-import androidx.compose.animation.shrinkVertically
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.BatteryUnknown
-import androidx.compose.material.icons.automirrored.filled.DirectionsBike
-import androidx.compose.material.icons.automirrored.filled.Dvr
-import androidx.compose.material.icons.filled.BatteryChargingFull
-import androidx.compose.material.icons.filled.BatteryUnknown
-import androidx.compose.material.icons.filled.ElectricBike
-import androidx.compose.material.icons.filled.ElectricalServices
-import androidx.compose.material.icons.filled.ExpandLess
-import androidx.compose.material.icons.filled.ExpandMore
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.LocalFireDepartment
-import androidx.compose.material3.*
+import androidx.compose.material.icons.rounded.LocationSearching
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.Gray
-import androidx.compose.ui.graphics.Color.Companion.Red
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.PermissionStatus
-import com.google.android.gms.maps.model.LatLng
-import com.ylabz.basepro.applications.bike.features.main.ui.BikeEvent
-import com.ylabz.basepro.applications.bike.features.main.ui.components.home.dials.StatsSection
-import com.ylabz.basepro.applications.bike.features.main.ui.components.home.main.SpeedAndProgressCard
-import com.ylabz.basepro.applications.bike.features.main.ui.components.home.main.StatItem
-import com.ylabz.basepro.applications.bike.features.main.ui.components.home.main.StatsRow
-import com.ylabz.basepro.applications.bike.features.main.ui.components.home.dials.bike.BikeBatteryLevels
-import com.ylabz.basepro.core.model.bike.BikeRideInfo
-import com.ylabz.basepro.core.model.bike.RideState
-
 import com.google.accompanist.permissions.rememberPermissionState
 
 @OptIn(ExperimentalPermissionsApi::class)
@@ -147,11 +124,63 @@ fun WaitingForGpsScreen(
         )
     )
 
+    val tintTrack by infinite.animateColor(
+        initialValue = Color(0xFF009688),//MaterialTheme.colorScheme.primary,
+        targetValue  = Color(0xFF673AB7),//MaterialTheme.colorScheme.secondary,
+        animationSpec = infiniteRepeatable(
+            animation = tween(1200, easing = LinearEasing),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+
+    val tintIn by infinite.animateColor(
+        initialValue = Color(0xFFF44336),//MaterialTheme.colorScheme.primary,
+        targetValue  = Color(0xFFFFC107),//MaterialTheme.colorScheme.secondary,
+        animationSpec = infiniteRepeatable(
+            animation = tween(1200, easing = LinearEasing),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+
+    val tintOut by infinite.animateColor(
+        initialValue = Color(0xFFFFC107),//MaterialTheme.colorScheme.primary,
+        targetValue  = Color(0xFFFFEB3B),//MaterialTheme.colorScheme.secondary,
+        animationSpec = infiniteRepeatable(
+            animation = tween(1200, easing = LinearEasing),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+
     Box(modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
         Column(horizontalAlignment = Alignment.CenterHorizontally) {
-            Text("Waiting for GPS…")
 
-            Spacer(Modifier.height(16.dp))
+
+            Text("Waiting for GPS…")
+            // Animated icon
+            Box (modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                Icon(
+                    imageVector = Icons.Rounded.LocationSearching,
+                    contentDescription = "Waiting for GPS",
+                    tint = tintIn,
+                    modifier = Modifier
+                        .size(90.dp)
+                        .graphicsLayer {
+                            rotationZ = rotation
+                            scaleX = scale
+                            scaleY = scale
+                        }
+                )
+                // finally: permission granted & GPS on
+                CircularProgressIndicator(
+                    strokeWidth = 12.dp,
+                    color = tintOut,//MaterialTheme.colorScheme.primary,
+                    //tint = tintIn,
+                    trackColor = tintIn,//MaterialTheme.colorScheme.secondary,
+                    modifier = Modifier.size(171.dp)
+                )
+            }
+            Spacer(Modifier.height(8.dp))
+            Text("Acquiring your location…")
 
             // 1) Ask for the permission
             if (!hasPermission) {
@@ -172,11 +201,14 @@ fun WaitingForGpsScreen(
                 }
                 return@Column
             }
-
-            // finally: permission granted & GPS on
-            CircularProgressIndicator()
-            Spacer(Modifier.height(8.dp))
-            Text("Acquiring your location…")
         }
     }
 }
+
+@RequiresPermission(allOf = [Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION])
+@Preview
+@Composable
+fun WaitingForGpsScreenPreview() {
+    WaitingForGpsScreen(onRequestPermission = {}, onEnableGpsSettings = {})
+}
+
