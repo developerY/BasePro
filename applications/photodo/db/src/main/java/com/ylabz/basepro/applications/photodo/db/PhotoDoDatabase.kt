97050f54f5a11fb8356a05d27c6ccb5835c6a5b2
Ash <developer@ylabz.com>
2025-11-30 15:03:06 -0800

    feat(db): Add TaskItemEntity to the database schema
    This commit updates the `PhotoDoDB` Room database definition to include the `TaskItemEntity`.
    
    The `entities` array in the `@Database` annotation has been modified to register `TaskItemEntity` as a new table, expanding the database schema to support task items.

--- applications/photodo/db/src/main/java/com/ylabz/basepro/applications/photodo/db/PhotoDoDatabase.kt
+++ applications/photodo/db/src/main/java/com/ylabz/basepro/applications/photodo/db/PhotoDoDatabase.kt
@@ -4,18 +4,20 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.ylabz.basepro.applications.photodo.db.entity.CategoryEntity
 import com.ylabz.basepro.applications.photodo.db.entity.PhotoEntity
+import com.ylabz.basepro.applications.photodo.db.entity.TaskItemEntity
 import com.ylabz.basepro.applications.photodo.db.entity.TaskListEntity
 
 @Database(
     entities = [
         CategoryEntity::class,
         TaskListEntity::class,
-        PhotoEntity::class
+        PhotoEntity::class,
+        TaskItemEntity::class // <--- ADD THIS
     ],
     version = 1,
     exportSchema = false
 )
-abstract class PhotoDoDB : RoomDatabase() {
 
+abstract class PhotoDoDB : RoomDatabase() {
     abstract fun photoDoDao(): PhotoDoDao
 }
