115939d6c3f860a804db5947c3f984f34d933e4d
developer <developer@ylabz.com>
2026-01-25 15:03:16 -0800

    refactor(settings): update Hilt import and streamline state handling
    This commit updates the `SettingsUiRoute.kt` file to use the modern Hilt lifecycle ViewModel artifact and refactors the UI state handling.
    
    The Hilt ViewModel import has been changed from `androidx.hilt.navigation.compose.hiltViewModel` to `androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel`.
    
    Additionally, the `when` statement has been streamlined by collecting and assigning the `uiState` from the `viewModel` in a single line.

--- feature/settings/src/main/java/com/ylabz/basepro/feature/settings/ui/SettingsUiRoute.kt
+++ feature/settings/src/main/java/com/ylabz/basepro/feature/settings/ui/SettingsUiRoute.kt
@@ -3,7 +3,7 @@ package com.ylabz.basepro.feature.settings.ui
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import com.ylabz.basepro.feature.settings.ui.components.ErrorScreen
 import com.ylabz.basepro.feature.settings.ui.components.LoadingScreen
 import com.ylabz.basepro.feature.settings.ui.components.SettingsCompose
@@ -15,9 +15,7 @@ fun SettingsUiRoute(
     navTo: (String) -> Unit,
     viewModel: SettingsViewModel = hiltViewModel()
 ) {
-    val uiState = viewModel.uiState.collectAsState().value
-
-    when (uiState) {
+    when (val uiState = viewModel.uiState.collectAsState().value) {
         is SettingsUiState.Loading -> {
             LoadingScreen()
         }
